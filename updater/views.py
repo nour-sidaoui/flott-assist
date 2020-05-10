49ef3b495b7671bf0a2491e6e7f4c95112be1003
nour-sidaoui <nour.sidaoui@gmail.com>
2020-05-10 16:32:23 +0200

    moving-up sys.path added for debug (views.py)

--- updater/views.py
+++ updater/views.py
@@ -6,14 +6,15 @@ import git
 
 @csrf_exempt
 def update(request):
+    import sys
+    print('\n'.join(sys.path))
+
     if request.method == "POST":
         '''
         pass the path of the diectory where your project will be 
         stored on PythonAnywhere in the git.Repo() as parameter.
         Here the name of my directory is "test.pythonanywhere.com"
         '''
-        import sys
-        print('\n'.join(sys.path))
 
         repo = git.Repo("sidaoui.pythonanywhere.com/")
         origin = repo.remotes.origin
