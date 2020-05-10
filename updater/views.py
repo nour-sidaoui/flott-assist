c33942daba754ede0663ae2068c3696391f004c1
nour-sidaoui <nour.sidaoui@gmail.com>
2020-05-10 18:47:36 +0200

    comments removed from updater

--- updater/views.py
+++ updater/views.py
@@ -6,12 +6,6 @@ from git import Repo
 @csrf_exempt
 def update(request):
     if request.method == "POST":
-        '''
-        pass the path of the diectory where your project will be 
-        stored on PythonAnywhere in the git.Repo() as parameter.
-        Here the name of my directory is "test.pythonanywhere.com"
-        '''
-
         repo = Repo("flott-assist/")
         origin = repo.remotes.origin
 
