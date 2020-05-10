94da68d4be4ea8b193c6e57b0f324ff5c66e7f53
nour-sidaoui <nour.sidaoui@gmail.com>
2020-05-10 14:29:06 +0200

    git.Repo updated in views.py

--- updater/views.py
+++ updater/views.py
@@ -12,7 +12,7 @@ def update(request):
         stored on PythonAnywhere in the git.Repo() as parameter.
         Here the name of my directory is "test.pythonanywhere.com"
         '''
-        repo = git.Repo("sidaoui.pythonanywhere.com//")
+        repo = git.Repo("sidaoui.pythonanywhere.com/home/sidaoui/flott-assist")
         origin = repo.remotes.origin
 
         origin.pull()
