061682fa51dab0627acb3fd8ee5156116cf0d996
nour-sidaoui <nour.sidaoui@gmail.com>
2020-05-10 14:37:35 +0200

    removed 'https://' to git.Repo views.py

--- updater/views.py
+++ updater/views.py
@@ -12,7 +12,7 @@ def update(request):
         stored on PythonAnywhere in the git.Repo() as parameter.
         Here the name of my directory is "test.pythonanywhere.com"
         '''
-        repo = git.Repo("https://sidaoui.pythonanywhere.com/")
+        repo = git.Repo("sidaoui.pythonanywhere.com/")
         origin = repo.remotes.origin
 
         origin.pull()
