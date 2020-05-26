820a39bf7551ac535ada68e9933a98ad277e1859
nour-sidaoui <nour.sidaoui@gmail.com>
2020-05-26 11:06:45 +0200

    "81.249.202.21" added to CORS_ORIGIN_WHITELIST

--- flott_assist/settings.py
+++ flott_assist/settings.py
@@ -76,7 +76,8 @@ MIDDLEWARE = [
 
 CORS_ORIGIN_WHITELIST = [
     "http://localhost:8100",
-    "http://127.0.0.1:8100"
+    "http://127.0.0.1:8100",
+    "81.249.202.21"
 ]
 
 CORS_ALLOW_METHODS = [
