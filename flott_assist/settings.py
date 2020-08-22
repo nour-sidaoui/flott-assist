6ee17376e60d068a6a7e9195d1ceaa21e00a67dc
nour-sidaoui <nour.sidaoui@gmail.com>
2020-08-22 21:39:36 +0200

    proportional fonts size limited for big screens in CSS and STATIC ROOT linked to DEBUG

--- flott_assist/settings.py
+++ flott_assist/settings.py
@@ -174,18 +174,26 @@ USE_THOUSAND_SEPARATOR = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.0/howto/static-files/
-
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+STATIC_URL = '/static/'
+
+if DEBUG:
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static')
+    ]
+
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static')
+# ]
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
